9c82f93e8af413c685c6ddf924086ce5c2b89677
naxiong <naxiong@pinduoduo.com>
2025-11-27 22:48:06 +0800

    update

--- python/fxcm_connector.py
+++ python/fxcm_connector.py
@@ -1,3 +1,16 @@
 import time
 from forexconnect import fxcorepy, ForexConnect
 
+class FXCMCLient:
+    def __init__(self, username, password, connection, url):
+        self.username = username
+        self.password = password
+        self.connection = connection # "Demo" or "Real"
+        self.url = url
+        self.client = ForexConnect()
+        self.session = None
+    
+    def connect(self):
+        print("Connecting to FXCM...")
+        try:
+            self.client.login(self.username, self.password, self.url, self.connection)
